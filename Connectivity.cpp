f440b0a613e074565dbe98041075cd22db7c055a
JPOliverio <348jpo@gmail.com>
2023-07-11 00:17:55 -0400

    all

--- Connectivity.cpp
+++ Connectivity.cpp
@@ -6,9 +6,45 @@ using namespace std;
 
 class Connectivity: public BeamElement{
     private:
+    list<int> firstNode;
+    list<int> secoundNode;
+    vector<double> displacement;
+    vector<double> load;
 
 
     public:
+    Connectivity(int node1, int node2){
+        firstNode.push_back(node1);
+        secoundNode.push_back(node2);
+    }
+
+    void displacmentVector(int numberOfNodes){
+        //the times two is because each node has two degree of freedoms
+        displacement = vector<double>(numberOfNodes*2,0); 
+
+    }
+
+    void loadVector(int numberOfNodes){
+        //the times two is because each node has two degree of freedoms
+        load = vector<double>(numberOfNodes*2,0); 
+
+    }
+
+    //the first value is the displacment in the y direction and the secound the x. negitive is down
+    void setDisplacment(int node, double disValue){
+        displacement[node] = disValue;
+    }
+
+    //the first value is the load in the y direction and the secound the x. negitive is down
+    void setLoad(int node, double loadValue){
+        load[node] = loadValue;
+
+    }
+
+
+
+
+
 
 
 
--- TrusGlobalStiffMatrix.cpp
+++ TrusGlobalStiffMatrix.cpp
@@ -1,10 +1,15 @@
 
+#include <vector>
 
+using namespace std;
 
 class TrusGlobalStiffMatrix{
 
     public:
-    TrusGlobalStiffMatrix(int){
+    TrusGlobalStiffMatrix(int numberOfNodes){
+        vector<vector<double>> globalStiffnessMatrix = vector<vector<double>>(numberOfNodes*2,
+         vector<double>(numberOfNodes*0, 0.0));
+
         
     }
 
